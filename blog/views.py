d74cf58efcec6966ce289719dee14e47f16b843d
kabdurahmonov <abdurahmonovk392@gmail.com>
2026-03-25 23:46:34 +0500

    rendered articles from database, added detail page for each article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Article
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
+def index(request):
+    articles = Article.objects.all()
+    return render(request, "blog/index.html", {"articles":articles})
+
+def detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, "blog/detail.html", {"article":article})
